19a067c92896c1637d3f6fe2bbfa30a5fd410a66
KINGSLY1998 <43057377+KINGSLY1998@users.noreply.github.com>
2018-10-04 11:42:20 +0530

    Add files via upload

---
+++ odd or even.java
@@ -0,0 +1,10 @@
+class Main {
+  public static void main(String[] args) {
+    int n=8;
+    System.out.println("Enter the Integer Number:");
+    if(n==8)
+    System.out.println("even");
+    else
+   System.out.println("odd"); 
+  }
+}
