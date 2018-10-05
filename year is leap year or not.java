2682e9bbecd950bd76726c5d89992a318f7adb47
KINGSLY1998 <43057377+KINGSLY1998@users.noreply.github.com>
2018-10-05 11:29:51 +0530

    Add files via upload

---
+++ year is leap year or not.java
@@ -0,0 +1,10 @@
+public class Main {
+   public static void main(String[] args)
+    {
+       int year = 2017;         
+       if(year % 4 == 0)
+               System.out.println("Year " + year + " is a leap year");
+       else if(year % 4 !=0)
+               System.out.println("Year " + year + " is not a leap year");      
+}
+}
